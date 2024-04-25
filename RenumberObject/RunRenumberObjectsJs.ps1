311b2b4af84fc3fd5dcfa1989199ea5b0e5b9889
EduardMartinezARQ <eduard.martinez@arquiconsult.com>
2024-04-25 16:55:06 +0200

    add powershell script

---
+++ RenumberObject/RunRenumberObjectsJs.ps1
@@ -0,0 +1,14 @@
+Param(
+    [Parameter(HelpMessage = "The Path Project", Mandatory = $false)]
+    [string] $projectPath
+)
+
+Write-Host "::group::Rename All Objects Id"
+Write-Host "Script path: $PSScriptRoot"
+Write-Host "Project path: $projectPath"
+# Rename All Objects Id
+$RenameAllObjectsPath = (Join-Path -Path $PSScriptRoot -ChildPath "..\RenumberObject\renumberObjects.js" -Resolve)
+Write-Host "Translation script path: $CreateTranslationScriptPath"
+Write-Host "Renamed all fields..."
+& 'C:\Program Files\nodejs\node.exe' $RenameAllObjectsPath $projectPath
+Write-Host "::endgroup::"
