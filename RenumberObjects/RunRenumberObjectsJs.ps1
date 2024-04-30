366403778f05b7a4dd1d6894e4a1af4de35ae53f
EduardMartinezARQ <eduard.martinez@arquiconsult.com>
2024-04-30 09:18:40 +0200

    update Powershell script

--- RenumberObjects/RunRenumberObjectsJs.ps1
+++ RenumberObjects/RunRenumberObjectsJs.ps1
@@ -10,11 +10,11 @@ $projectPathCompleted = Join-Path $baseFolder $ProjectPath
 
 Write-Host "::group::Rename All Objects Id"
 Write-Host "Script path: $PSScriptRoot"
-Write-Host "Project path: $projectPath"
+Write-Host "Project path: $projectPathCompleted"
 Write-Host $projectPath
 # Rename All Objects Id
 $RenameAllObjectsPath = (Join-Path -Path $PSScriptRoot -ChildPath "..\RenumberObjects\renumberObjects.js" -Resolve)
-Write-Host "Translation script path: $RenameAllObjectsPath"
+Write-Host "Rename Objects script path: $RenameAllObjectsPath"
 Write-Host "Renamed all fields..."
 & 'C:\Program Files\nodejs\node.exe' $RenameAllObjectsPath $projectPathCompleted
 Write-Host "::endgroup::"
