a32310a6d1b0547007676b4234ddc33daf08c4d4
EduardMartinezARQ <eduard.martinez@arquiconsult.com>
2024-04-30 08:55:53 +0200

    update Powershell script

--- RenumberObjects/RunRenumberObjectsJs.ps1
+++ RenumberObjects/RunRenumberObjectsJs.ps1
@@ -1,11 +1,17 @@
 Param(
     [Parameter(HelpMessage = "The Path Project", Mandatory = $false)]
-    [string] $projectPath
+    [string] $project = ""
 )
+
+$projectPath = $null
+
+$baseFolder = $ENV:GITHUB_WORKSPACE
+$projectPath = Join-Path $baseFolder $project
 
 Write-Host "::group::Rename All Objects Id"
 Write-Host "Script path: $PSScriptRoot"
 Write-Host "Project path: $projectPath"
+Write-Host $projectPath
 # Rename All Objects Id
 $RenameAllObjectsPath = (Join-Path -Path $PSScriptRoot -ChildPath "..\RenumberObjects\renumberObjects.js" -Resolve)
 Write-Host "Translation script path: $RenameAllObjectsPath"
